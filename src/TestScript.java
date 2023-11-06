362d9d4b71c9984dfbdb5d7ac50e547bbd712a9d
devvv <dsevrens@gmail.com>
2023-11-06 00:36:37 -0800

    Added manifest

--- src/TestScript.java
+++ src/TestScript.java
@@ -1,6 +1,10 @@
 import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.script.Category;
+import org.dreambot.api.script.ScriptManifest;
 import org.dreambot.api.utilities.Logger;
 
+@ScriptManifest(name = "Script Name", description = "My script description!", author = "Developer Name",
+        version = 1.0, category = Category.UTILITY, image = "")
 public class TestScript extends AbstractScript {
 
     @Override
